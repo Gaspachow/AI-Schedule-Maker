eaa5e214b2d235f055a0690fe070e117adfb73c9
gaspachow <gaspardsmets@gmail.com>
2020-05-06 16:46:56 +0200

    Calendar linked to values

--- js/front.js
+++ js/front.js
@@ -3,7 +3,8 @@ document.getElementById("create-button").addEventListener("click", nextStep);
 function  nextStep() {
     document.getElementById("form_1").style.display = "none";
     document.getElementById("create-button").style.display = "none";
-    document.getElementById("weekCalendar").style.display = "flex";
+    document.getElementById("finalResult").style.display = "flex";
+    makeCalendar();
 }
 
 var hourHTML = "<option value=0>00</option><option value=1>01</option><option value=2>02</option><option value=3>03</option><option value=0>04</option><option value=5>05</option><option value=6>06</option><option value=7>07</option><option value=8>08</option><option value=9>09</option><option value=10>10</option><option value=11>11</option><option value=12>12</option><option value=13>13</option><option value=14>14</option><option value=15>15</option><option value=16>16</option><option value=17>17</option><option value=18>18</option><option value=19>19</option><option value=20>20</option><option value=21>21</option><option value=22>22</option><option value=23>23</option>" ;
@@ -18,7 +19,26 @@ function makeHourInputs() {
     console.log("Hours and Minutes divisions filled!");
 }
 
-function squareCreate() {
+function getSchedule() {
+    var workStartHour = document.getElementById("startwork-h").value;
+    var workStartMin = document.getElementById("startwork-m").value;
+    var workEndHour = document.getElementById("stopwork-h").value;
+    var workEndMin = document.getElementById("stopwork-m").value;
+    var workArray = [document.getElementById("workDay0").checked,
+                     document.getElementById("workDay1").checked,
+                     document.getElementById("workDay2").checked,
+                     document.getElementById("workDay3").checked,
+                     document.getElementById("workDay4").checked,
+                     document.getElementById("workDay5").checked,
+                     document.getElementById("workDay6").checked];
+    
+    var schedule = generateSchedule(workStartHour, workStartMin, workEndHour, workEndMin, workArray);
+    return schedule;
+}
+
+function makeCalendar() {
+    var schedule = getSchedule();
+
     var calendarDiv = document.getElementById("weekCalendar");
     for(i=0;i<=6;i++){
       let divCreateVer = document.createElement('div');
@@ -35,9 +55,11 @@ function squareCreate() {
             divCreateMin.classList.add('minutes');
             divCreateMin.id = ('m' + i + '.' + j + '.' + k);
             divCreateHor.appendChild(divCreateMin);
-            divCreateMin.innerHTML = divCreateMin.id;
+            divCreateMin.classList.add('task' + schedule[i][j][k]);
         }
-    }}}
+      }
+    }
+    console.log("Calendar Made!");
+}
 
 document.getElementById("body").onload = makeHourInputs();
-document.getElementById("body").onload = squareCreate();
